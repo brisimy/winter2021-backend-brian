298e0f8a629824d9e272886a02265817bef37d00
Brian Simpson <brisimy@gmail.com>
2021-11-09 11:51:22 -0800

    update

--- week3/web_server/index.js
+++ week3/web_server/index.js
@@ -7,8 +7,14 @@ const app = express();
 
 const http = require("http").Server(app);
 
+const port = 3000;
+
 // Tells Express/HTTP what port to listen to. 
 
-http.listen(3000);
+http.listen(port); //Common Development Ports: 8080, 3000 = Node.
 
-console.log("Express server is running on port 3000.");
+console.log("Express server is running on port " + port + ".");
+
+//Express Routes. Allow us to "redirect" a URL filepath to a specific folder. 
+
+app.use("/" , express.static("public_html/"));
