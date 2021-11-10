4e7b8b27b00e1e6f596819578995e671fd3c3778
Brian Simpson <brisimy@gmail.com>
2021-11-10 11:53:06 -0800

    update index html and js

--- week3/web_server/index.js
+++ week3/web_server/index.js
@@ -1,9 +1,17 @@
 // Use the Express package. 
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
 const app = express();
 
 // Allow Express to use the HTTP module of Node. 
+
+const http = require("http").Server(app);
+
+app.use (express.json({strict:false}));
+app.use (express.json({extended:true}));
+
 
 const http = require("http").Server(app);
 
@@ -13,8 +21,16 @@ const port = 3000;
 
 http.listen(port); //Common Development Ports: 8080, 3000 = Node.
 
-console.log("Express server is running on port " + port + ".");
+//console.log("Express server is running on port " + port + ".");
 
 //Express Routes. Allow us to "redirect" a URL filepath to a specific folder. 
 
 app.use("/" , express.static("public_html/"));
+
+app.post("/test", function(request, response){
+
+let dataFromRequest = JSON.parse(request.body);
+console.log("The front end send the follow: ", + request); 
+
+response.sendStatus(200);
+});
